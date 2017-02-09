80cb0e04280e199afb164686a0ad39e93ee7a878
jurg <jurg@jrgens-mbp.lan>
2017-02-09 23:57:45 +0100

    ferdigcommit

--- DAT102_Oving1/src/no/hib/dat102/CDArkivKlient.java
+++ DAT102_Oving1/src/no/hib/dat102/CDArkivKlient.java
@@ -4,7 +4,7 @@ import no.hib.dat102.adt.CDarkivADT;
 public class CDArkivKlient {
 
 	public static void main(String[] args) {
-		CDarkivADT cda = new CDarkiv2(10);
+		CDarkivADT cda = new CDarkiv();
 		Meny meny = new Meny(cda);
 		meny.start();
 	}
